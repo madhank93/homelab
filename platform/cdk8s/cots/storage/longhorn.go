1372bd7ce1470a1cacd9a30f4396b909d0cbed2d
Madhan <madhankumaravelu93@gmail.com>
2026-02-13 13:36:40 -0600

    fix: downgrade longhorn to v1.10.2 (stable)

--- platform/cdk8s/cots/storage/longhorn.go
+++ platform/cdk8s/cots/storage/longhorn.go
@@ -72,7 +72,7 @@ func NewLonghornChart(scope constructs.Construct, id string, namespace string) c
 	cdk8s.NewHelm(chart, jsii.String("longhorn-release"), &cdk8s.HelmProps{
 		Chart:       jsii.String("longhorn"),
 		Repo:        jsii.String("https://charts.longhorn.io"),
-		Version:     jsii.String("1.8.0"),
+		Version:     jsii.String("1.10.2"),
 		ReleaseName: jsii.String("longhorn"),
 		Namespace:   jsii.String(namespace),
 		Values:      &values,
